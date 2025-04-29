9d9670eb666bdd81ed874f3d9d81bac78266ab2a
Sumi Seo <sumseo@bess-f2r1s18.clusters.42paris.fr>
2025-04-29 15:29:41 +0200

    feat/finalize-sign-in-sign-out

--- frontend/src/googleAuth/initAuth.ts
+++ frontend/src/googleAuth/initAuth.ts
@@ -1,19 +1,47 @@
 const client_id = import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID;
 
-function handleCredentialResponse(response: any) {
-	// console.log("Encoded JWT ID token: " + response.credential);
-	console.log("RESPONSE FROM GOOGLE LOGIN", response);
-    // I have to find correct google element to delete it after login
-    const popup = document.getElementById(".loginPopup");
-    if (popup) {
-        popup.style.display = "none";  // Hide the element if it exists
-      }
-   
+
+const decodeJWTToken = (token : string) => {
+	const base64Url = token.split('.')[1];
+	const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	const jsonPayload = decodeURIComponent(
+		atob(base64)
+			.split('')
+			.map((c) => {
+				return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+			})
+			.join('')
+	);
+
+	const decoded = JSON.parse(jsonPayload);
+	/* payload 
+		family_name: "EXAMPLE"
+		given_name: "EXAMPLE"
+		email: "example@gmail.com"
+	*/
+	console.log("payload", decoded)
+	return decoded.email;
 }
 
-function loadGoogleSignInScript(): Promise<void> {
+
+function handleCredentialResponse(response: any) {
+	// console.log("RESPONSE FROM GOOGLE LOGIN", response.credential);
+	if(response.credential)
+	{
+		const email = decodeJWTToken(response.credential);
+		localStorage.setItem("googleSignInEmail", email);
+		const popup = document.getElementById("loginPopup");
+		if (popup) {
+			popup.style.display = "none";  // Hide google popup login
+		}
+	}
+	
+}
+
+export function loadGoogleSignInScript(): Promise<void> {
 	return new Promise((resolve, reject) => {
 	  const existingScript = document.querySelector('script[src="https://accounts.google.com/gsi/client"]');
+	
 	  if (existingScript) {
 		resolve(); // Script is already loaded
 		return;
@@ -37,55 +65,63 @@ function loadGoogleSignInScript(): Promise<void> {
 	});
   }
   
-function initGoogleAuth(islogIn:boolean) {
+export function initGoogleAuth() {
 
-	loadGoogleSignInScript()
-	  .then(() => {
-		// Make sure `google` is available
-		if (typeof google !== 'undefined') {
-		  google.accounts.id.initialize({
-			client_id: client_id,
-			callback: handleCredentialResponse,
-		  });
-
-          const popup = document.createElement("div");
-          popup.setAttribute("id", "loginPopup");
-          popup.setAttribute("class", "px-20 py-20");
-          document.body.appendChild(popup);
-          popup.style.position = "absolute";
-          popup.style.top = "50px";
-          popup.style.left = "50%"; 
-          popup.style.transform = "translateX(-50%)"; 
-    
-		  google.accounts.id.renderButton(
-			document.getElementById("loginPopup")!,
-			{ theme: "outline", size: "large" }
-		  );
-          if(islogIn)
-          {
-            google.accounts.id.prompt(); // Optional: show One Tap prompt
-          }
-		   
-          else
-          {
-            const button = document.getElementById('logoutButton');
-            console.log(button);
-            if (button) {
-                button.onclick = () => {
-                    google.accounts.id.revoke().then(() => {
-                        console.log('Successfully logged out');
-                        // Optionally, you could also hide the button or show a "logged out" state here
-                    }).catch((error: any) => {
-                        console.error('Error during logout', error);
-                    });
-                };
-            }
-          }
-		} else {
-		  console.error("Google object not found after loading script.");
+	// Check user sign-in state
+	const email = localStorage.getItem("googleSignInEmail");
+	if(email)
+	{
+		console.log("There is already user");
+		return;
+	}
+	// if there is not user signed-in, google pop up happens
+	else
+	{
+		if (typeof google !== 'undefined') 
+		{
+			google.accounts.id.initialize({
+				client_id: client_id,
+				callback: handleCredentialResponse,
+			});
 		}
-	  })
-	  .catch(console.error);
-  }
-  
-  export default initGoogleAuth;
+			
+		const popup = document.createElement("div");
+		popup.setAttribute("id", "loginPopup");
+		popup.setAttribute("data-size", "medium");
+		document.body.appendChild(popup);
+		popup.style.position = "absolute";
+		popup.style.top = "50%";
+		popup.style.left = "50%"; 
+		popup.style.transform = "translateX(-50%)"; 
+    
+	
+		// Ask users to login with Google
+		google.accounts.id.renderButton(
+			document.getElementById("loginPopup")!,
+			{ theme: "filled_black", size: "large" }
+		);
+		// Ask confirmatin to login with Google
+		google.accounts.id.prompt(); // Optional: show One Tap prompts
+	}
+}
+
+export function setupLogoutButton() {
+	const button = document.getElementById("logoutButton");
+	if (button) {
+		button.onclick = () => {
+			const email = localStorage.getItem("googleSignInEmail");
+			if (!email) {
+				console.log("No email found for logout");
+				return;
+			}
+
+			if (typeof google !== 'undefined') {
+				// This just removes the remembered selection
+				google.accounts.id.disableAutoSelect();
+				console.log("Logged out (email cleared)");
+				localStorage.removeItem("googleSignInEmail");
+			}
+		};
+	}
+}
+
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 import i18n from './i18n/i18n';
-import initGoogleAuth from './googleAuth/initAuth';"./googleAuth"
+import {initGoogleAuth, loadGoogleSignInScript, setupLogoutButton} from './googleAuth/initAuth';"./googleAuth"
 
 async function initApp() {
 	const app = document.getElementById('app')!;
@@ -8,6 +8,9 @@ async function initApp() {
 	try {
 		const response = await fetch('/demo.html');
 		const html = await response.text();
+
+		// Initialize Google Sign-In object
+		loadGoogleSignInScript();
 
 		// Inject the HTML content into the app element
 		app.innerHTML = html;
@@ -26,9 +29,9 @@ async function initApp() {
 			});
 		});
 
-		document.getElementById('loginButton')!.addEventListener('click', () => googleSignIn(true));
+		document.getElementById('loginButton')!.addEventListener('click', () => googleSignIn());
 		// document.getElementById('loginButton')!.addEventListener('click', () => handlePostRequest('/api/auth/login', ''));
-		document.getElementById("logoutButton")!.addEventListener("click",()=>googleSignIn(false));
+		document.getElementById("logoutButton")!.addEventListener("click",()=> setupLogoutButton());
 		document.getElementById('gameButton')!.addEventListener('click', () => handlePostRequest('/api/game/join', ''));
 		document.getElementById('checkUsernameButton')!.addEventListener('click', () => {
 			const username = (document.getElementById('usernameInput') as HTMLInputElement).value;
@@ -51,8 +54,8 @@ function translateDOM() {
 }
 
 
-async function googleSignIn(islogIn: boolean){
-	initGoogleAuth(islogIn);
+async function googleSignIn(){
+	initGoogleAuth();
 }
 
 async function googleSignOut(){
