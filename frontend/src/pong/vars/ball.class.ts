ffe6f760fef42fc0695e6b028b3723475803d440
raph2leretour <tisserandraph@gmail.com>
2025-05-26 18:08:51 +0200

    ball moves set but not tested

--- frontend/src/pong/vars/ball.class.ts
+++ frontend/src/pong/vars/ball.class.ts
@@ -4,9 +4,39 @@ import { Paddle } from "./paddle.class.js";
 export class Ball extends GameElement {
 	constructor(x: number, y: number, h: number, w: number, s: number) {
 		super(x, y, h, w, s);
+		this.xVec = 1;
+		this.yVec = 1;
 	}
 
 	update(player1: Paddle, player2: Paddle, canvas) {
+		if (this.xVec > 0) {
+			if (this.x + 1 >= player1.x - player1.width) {
+				this.xVec *= -1;
+			}
+			if (this.yVec > 1) {
+				if (this.y + 1 >= canvas.height) {
+					this.yVec *= -1;
+				}
+			} else {
+				if (this.y - 1 <= canvas.height) {
+					this.yVec *= -1;
+				}
+			}
+		} else {
+			if (this.x + 1 <= player2.x) {
+				this.xVec *= -1;
+			}
+			if (this.yVec < 1) {
+				if (this.y + 1 <= canvas.height) {
+					this.yVec *= -1;
+				}
+			} else {
+				if (this.y - 1 >= canvas.height) {
+					this.yVec *= -1;
+				}
+			}
+		}
+
 		this.x += this.xVec * this.speed;
 		this.y += this.yVec * this.speed;
 	}
