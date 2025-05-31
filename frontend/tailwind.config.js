5ac93522eadbc5ebd9c9febadf6d9aaaf2a8e01c
HugooVer <hugo.vercelloneg@gmail.com>
2025-05-31 21:39:55 +0200

    feat: workig language selection menu and enhance layout in home template; update Tailwind configuration for infinite scroll animation; add "infinit scroll" in the the home page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
 			fontFamily: {
 				sans: ['Russo One','Black Han Sans', 'system-ui', 'sans-serif'],
 			},
+			keyframes: {
+				'infinite-scroll': {
+				'0%': { transform: 'translateX(0)' },
+				'100%': { transform: 'translateX(-50%)' },
+				},
+		},
+			animation: {
+				'infinite-scroll': 'infinite-scroll 15s linear infinite',
+			},
 		},
 	},
 	plugins: [],
