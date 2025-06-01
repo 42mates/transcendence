481e99e6fc8645db5fe25891945ff5be7f8bd36c
raph2leretour <tisserandraph@gmail.com>
2025-06-01 18:29:19 +0200

    updating Paddle to send message (pending)

--- services/game/src/pong/vars/paddle.class.ts
+++ services/game/src/pong/vars/paddle.class.ts
@@ -1,6 +1,7 @@
-import { ConnectedUser } from "../../types/GameMessages.js";
+import { ConnectedUser, GameStateMessage } from "../../types/GameMessages.js";
 import { GameCanvas } from "./gameCanvas.class.js";
 import { Template } from "./template.class.js";
+import { Ball } from "./ball.class.js";
 
 export class Paddle extends Template {
 	down: boolean;
@@ -9,13 +10,13 @@ export class Paddle extends Template {
 	connectedUsers: ConnectedUser;
 	private speed: number;
 
-	constructor(x: number, y: number, h: number, w: number, c: ConnectedUser) {
+	constructor(x: number, y: number, h: number, w: number, cu: ConnectedUser) {
 		super(x, y, h, w);
 		this.up = false;
 		this.down = false;
 		this.score = 0;
 		this.speed = 10;
-		this.connectedUsers = c;
+		this.connectedUsers = cu;
 	}
 
 	update(gameCanvas: GameCanvas) {
@@ -31,5 +32,19 @@ export class Paddle extends Template {
 		}
 
 		this.y += this.speed * this.yVec;
+	}
+
+	sendUpdate(player2: Paddle, ball: Ball) {
+		const response: GameStateMessage = {
+			type: "game_state",
+			ball: { x: ball.x, y: ball.y },
+			paddles: [
+				{ x: player2.x, y: player2.y },
+				{ x: player2.x, y: player2.y },
+			],
+			score: this.score,
+			status: 
+		};
+		this.connectedUsers.ws.send(JSON.stringify(response));
 	}
 }
